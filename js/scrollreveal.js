096f34bd66b2afa432a8a3473e06558843d38c9e
Ricardo Magalhães <ricardospilotes@gmail.com>
2022-05-21 12:15:43 -0300

    Add files via upload

---
+++ js/scrollreveal.js
@@ -0,0 +1,25 @@
+ScrollReveal().reveal("#dia", {
+  origin: "top",
+  distance: "3rem",
+  duration: 4000,
+});
+
+ScrollReveal().reveal(".saudacao", {
+  duration: 5000,
+});
+
+ScrollReveal().reveal(".weather", {
+  duration: 5000,
+});
+
+ScrollReveal().reveal(".links-container", {
+  duration: 5000,
+});
+
+ScrollReveal().reveal(".logo", {
+  duration: 5000,
+});
+
+ScrollReveal().reveal(".infos", {
+  duration: 5000,
+});
