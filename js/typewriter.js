f44dcd3f5c637d475048db251d7641e29a6aae9c
Ricardo Magalhães <ricardospilotes@gmail.com>
2022-07-03 04:19:59 -0300

    Add files via upload

---
+++ js/typewriter.js
@@ -0,0 +1,10 @@
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = "";
+  for (let i = 0; i < textoArray.length; i++) {
+    setTimeout(() => (elemento.innerHTML += textoArray[i]), 210 * i);
+  }
+}
+
+const name = document.querySelector(".h3");
+typeWriter(name);
