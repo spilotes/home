ee9be1e705a6981fe4b90287d0e045e7b7237292
Ricardo Magalhães <ricardospilotes@gmail.com>
2022-07-03 04:21:50 -0300

    Add files via upload

--- js/typewriter.js
+++ js/typewriter.js
@@ -2,7 +2,7 @@ function typeWriter(elemento) {
   const textoArray = elemento.innerHTML.split("");
   elemento.innerHTML = "";
   for (let i = 0; i < textoArray.length; i++) {
-    setTimeout(() => (elemento.innerHTML += textoArray[i]), 210 * i);
+    setTimeout(() => (elemento.innerHTML += textoArray[i]), 220 * i);
   }
 }
 
